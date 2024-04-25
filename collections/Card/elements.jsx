d3d35dd5834959eb69497445f6573fc0255adc55
sasholilov <alex_xld@abv.bg>
2024-04-25 21:42:42 +0300

    update card style on hover

--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -5,22 +5,34 @@ import {
 } from "../../components/Typography/elements";
 
 export const StyledCardContainer = styled.div`
+  cursor: pointer;
+  box-sizing: border-box;
   background-color: #f1f1f1;
   max-width: ${(props) =>
     props.type === "mincard"
-      ? `300px`
+      ? `400px`
       : props.type === "midcard"
-      ? `330px`
+      ? `420px`
       : props.type === "largecard"
-      ? `360px`
-      : `300px`};
+      ? `440px`
+      : `400px`};
   padding: 1rem 2.5rem 1rem 2.5rem;
   border-radius: 6px;
+  border: 2px solid transparent;
+
+  & :hover {
+    border: 2px solid #0772fc;
+  }
 `;
 
 export const StyledCardHeading = styled(StyledSectionHeading)`
   font-size: 1.3em;
   line-height: 0;
+
+  ${StyledCardContainer}:hover & {
+    color: #0772fc;
+    text-decoration: underline;
+  }
 `;
 
 export const StyledCardDescription = styled(StyledSectionParagraph)``;
